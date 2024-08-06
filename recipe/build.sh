b9269b6bb80b8f408d159c9ad02e3d485ff239ed
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-08-06 11:47:43 +0200

    fix issues

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=true
+export CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
+
 # build statically linked binary with Rust
 cargo install --locked --features=all --root "$PREFIX" --path .
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/sccache"
 
 # remove extra build file
 rm -f "${PREFIX}/.crates.toml"
