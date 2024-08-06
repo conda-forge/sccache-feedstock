5e425e1f8f6649feb7871e1d9fc68227fd7b85b1
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-08-06 13:06:26 +0200

    fix: use --no-track

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,11 +3,7 @@ set "CARGO_PROFILE_RELEASE_LTO=true"
 set "CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1"
 
 :: build
-cargo install --locked --features=all --root "%PREFIX%" --path . || goto :error
-
-:: remove extra build file
-del /F /Q "%PREFIX%\.crates.toml"
-del /F /Q "%PREFIX%\.crates2.json"
+cargo install --locked --features=all --root "%PREFIX%" --path . --no-track || goto :error
 
 :: generate licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,7 @@ export CARGO_PROFILE_RELEASE_LTO=true
 export CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
 
 # build statically linked binary with Rust
-cargo install --locked --features=all --root "$PREFIX" --path .
-
-# remove extra build file
-rm -f "${PREFIX}/.crates.toml"
-rm -f "${PREFIX}/.crates2.json"
+cargo install --locked --features=all --root "$PREFIX" --path . --no-track
 
 # generate licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
